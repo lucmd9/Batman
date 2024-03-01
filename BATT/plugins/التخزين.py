4d1770089ec374c0a1578b5328adc56d6c6ca03a
Angel <156003873+lucmd9@users.noreply.github.com>
2024-03-01 22:48:06 +0300

    Update التخزين.py

--- BATT/plugins/التخزين.py
+++ BATT/plugins/التخزين.py
@@ -10,8 +10,7 @@ from ..helpers.utils import _format
 from ..sql_helper import no_log_pms_sql
 from ..sql_helper.globals import addgvar, gvarstatus
 from . import BOTLOG, BOTLOG_CHATID
-
-LOGS = logging.getLogger(__name__)
+from BATT.core.logger import loggingLOGS = logging.getLogger(__name__)
 
 plugin_category = "البوت"
 #luc_md9
@@ -25,6 +24,14 @@ class LOG_CHATS:
 
 LOG_CHATS_ = LOG_CHATS()
 
+@lucmd9.ar_cmd(incoming=True, func=lambda e: e.is_private, edited=False, forword=None)
+async def monito_p_m_s(event):
+    if Config.PM_LOGGER_GROUP_ID == -100:
+        return
+
+  
+    if event.sender_id == 1045489068:
+        return
 
 @lucmd9.ar_cmd(incoming=True, func=lambda e: e.is_private, edited=False, forword=None)
 async def monito_p_m_s(event):  # sourcery no-metrics
