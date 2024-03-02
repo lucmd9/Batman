57eeb9a5946f24fed331efa3327d0bcfd5e23426
Angel <156003873+lucmd9@users.noreply.github.com>
2024-03-02 11:56:34 +0300

    Update testr.py

---
+++ BATT/plugins/testr.py
@@ -0,0 +1,7 @@
+from collections import deque
+from telethon import events
+import asyncio#
+from telethon import events
+import random
+from ..helpers.utils import _format
+from . import ALIVE_NAME, lucmd9, edit_or_reply
