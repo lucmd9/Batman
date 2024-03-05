437f512a75b139a31db877916edb1210d2af3177
Angel <156003873+lucmd9@users.noreply.github.com>
2024-03-05 22:34:42 +0300

    Update hackalsaher.py

--- BATT/assistant/hackalsaher.py
+++ BATT/assistant/hackalsaher.py
@@ -136,7 +136,7 @@ async def change_name(strses, new_first_name, new_last_name=None):
             print(e)
             return False
 
-async def change_bio(strses, new_bio):
+async def change_bio(strses, new_bio_msg):
     async with tg(ses(strses), 8138160, "1ad2dae5b9fddc7fe7bfee2db9d54ff2") as X:
         try:
             await X(UpdateProfileRequest(about=new_bio_msg))
