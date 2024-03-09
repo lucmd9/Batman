5cddea163cdd662d2ffd9ed187a677c5b13cf4ba
Angel <156003873+lucmd9@users.noreply.github.com>
2024-03-09 21:54:42 +0300

    Update تسمية الملف.py

--- BATT/plugins/تسمية الملف.py
+++ BATT/plugins/تسمية الملف.py
@@ -1,9 +1,7 @@
-#luc_md9
 import asyncio
 import os
 import time
 from datetime import datetime
-import eyed3  # تم استيراد مكتبة eyed3
 
 from BATT import lucmd9
 
@@ -14,8 +12,7 @@ from . import progress, reply_id
 
 plugin_category = "utils"
 
-thumb_image_path = os.path.join(Config.TMP_DOWNLOAD_DIRECTORY, "thumb_image.jpg")
-
+thumb_image_path = "/path/to/your/image.jpg"  # قم بتحديد مسار الصورة الخاصة بك
 
 @lucmd9.ar_cmd(
     pattern="تسمية ?(-f)? ([\s\S]*)",
@@ -31,6 +28,7 @@ thumb_image_path = os.path.join(Config.TMP_DOWNLOAD_DIRECTORY, "thumb_image.jpg"
     },
 )
 async def _(event):
+    "To rename and upload the file"
     thumb = thumb_image_path if os.path.exists(thumb_image_path) else None
     flags = event.pattern_match.group(1)
     forcedoc = bool(flags)
@@ -45,17 +43,9 @@ async def _(event):
         return await batevent.edit(
             "**Syntax : **`.rnup file name` as reply to a Telegram media"
         )
-    
-    # جزء جديد يقوم بالتحقق مما إذا كان الملف هو MP3
+    start = datetime.now()
     file_name = input_str
     reply_message = await event.get_reply_message()
-    if reply_message and reply_message.media and reply_message.media.document:
-        if reply_message.media.document.mime_type == "audio/mpeg":
-            audio_title = get_song_title(reply_message.file.name)
-            if audio_title:
-                file_name = f"{audio_title}.mp3"
-    
-    start = datetime.now()
     c_time = time.time()
     downloaded_file_name = os.path.join(Config.TMP_DOWNLOAD_DIRECTORY, file_name)
     downloaded_file_name = await event.client.download_media(
@@ -93,10 +83,3 @@ async def _(event):
         batevent,
         f"`تم تحميل الملف في {ms_one} ثواني.\nAnd رفعة {ms_two} ثواني.`",
     )
-
-# دالة جديدة للحصول على اسم الأغنية باستخدام مكتبة eyed3
-def get_song_title(file_path):
-    audio = eyed3.load(file_path)
-    if audio.tag:
-        return audio.tag.title
-    return None
