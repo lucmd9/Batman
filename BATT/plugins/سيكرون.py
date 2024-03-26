59e1c73adc1413c008171dc96b6b330911e91f98
Angel <156003873+lucmd9@users.noreply.github.com>
2024-03-26 13:50:28 -0700

    Update سيكرون.py

--- BATT/plugins/سيكرون.py
+++ BATT/plugins/سيكرون.py
@@ -3,59 +3,56 @@
 
 
 import io
-import traceback
 from datetime import datetime
-from selenium import webdriver
-from telethon import events
-from BATT.events import lucmd9
-from BATT.utils import edit_or_reply
-CHROME = "/usr/bin/google-chrome"
+import requests
+from validators.url import url
+from BATT import lucmd9
+from ..Config import Config
+from ..core.managers import edit_delete, edit_or_reply
+from ..helpers.google_tools import chromeDriver
+from ..helpers.utils import reply_id
 
-@lucmd9.ar_cmd(pattern="سكرن (.*)") #خوش غلطه هنا جانت
+plugin_category = "utils"
+
+
+@lucmd9.ar_cmd(
+    pattern="(سكرنلي|سكرن)(?:\s|$)([\s\S]*)",
+    command=("سكرن", plugin_category),
+)
 async def _(event):
-    if event.fwd_from:
-        return
-   await edit_or_reply(event, "اصبر....")
-    start = datetime.now()
-    try:
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--ignore-certificate-errors')
-        chrome_options.add_argument("--test-type")
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.binary_location = CHROME
-        await edit_or_reply(event, "يتم تشغيل كوكل  bin")
-        with webdriver.Chrome(options=chrome_options) as driver:
-            input_str = event.pattern_match.group(1)
-            driver.get(input_str)#ولا راح يشتغل 
-        await edit_or_reply(event, "يتم تجميع..")
-        height = driver.execute_script("return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);")
-        width = driver.execute_script("return Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);")
-        await edit_or_reply(event, "Painting web-page")
-        driver.set_window_size(width + 100, height + 100)
-        im_png = driver.get_screenshot_as_png()
-        #هنا ينحفظ السكرين توضيح
-        driver.close()
-        await edit_or_reply(event, "يتم ايقاف كروم bin")
-        message_id = event.message.id
-        if event.reply_to_msg_id:
-            message_id = event.reply_to_msg_id
-        with io.BytesIO(im_png) as out_file:
-            out_file.name = "BATT.screenCapture.PNG"
-            await event.client.send_file(
-                event.chat_id,
-                out_file,
-                caption=input_str,
-                force_document=True,
-                reply_to=message_id,
-                allow_cache=False,
-                silent=True
-            )
-        end = datetime.now()
-        ms = (end - start).seconds
-        await edit_or_reply(event, f"تم التقاط السكرين استغرق حوالي {ms} sec")
-    except Exception:
-        await edit_or_reply(event,(traceback.format_exc())
+    cmd = event.pattern_match.group(1)
+    text = event.pattern_match.group(2)
+    reply = await event.get_reply_message()
+    reply_to_id = await reply_id(event)
+    if not text and reply:
+        text = reply.text
+    if not text:
+        return await edit_delete(event, "اصبر بعد اخوك ...")
+    if cmd == "s;vk":
+        caturl = url(text)
+        if not caturl:
+            text = f"http://{text}"
+            caturl = url(text)
+        if not caturl:
+            return await edit_delete(event, "همممم هذا الرابط غير مدعوم")
+    elif cmd == "سكرنلي":
+        text = f"https://www.google.com/search?q={text}"
+    batevent = await edit_or_reply(event, "يتم التحميل ...")
+    image, response = await chromeDriver.get_screenshot(text, batevent)
+    if not image:
+        return await edit_delete(batevent, response)
+
+    await batevent.delete()
+    with io.BytesIO(image) as out_file:
+        out_file.name = f"{text}.PNG"
+        await event.client.send_file(
+            event.chat_id,
+            out_file,
+            caption=response,
+            force_document=True,
+            reply_to=reply_to_id,
+            allow_cache=False,
+            silent=True,
+        )
         
       #جميع حقوق لسورس الخفاش تبوكه انيجك
