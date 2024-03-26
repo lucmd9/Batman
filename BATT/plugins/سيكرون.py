cd57f585e3b33dbb3220ed4016eb3ee86f5af840
Angel <156003873+lucmd9@users.noreply.github.com>
2024-03-26 14:10:12 -0700

    Update سيكرون.py

--- BATT/plugins/سيكرون.py
+++ BATT/plugins/سيكرون.py
@@ -1,7 +1,6 @@
 #امر تاخذ سكرين للمواقع
 #————————BATT—————————-#LUC_MD9
 
-
 import io
 from datetime import datetime
 import requests
@@ -16,10 +15,16 @@ plugin_category = "utils"
 
 
 @lucmd9.ar_cmd(
-    pattern="(سكرنلي|سكرن)(?:\s|$)([\s\S]*)",
-    command=("سكرن", plugin_category),
+    pattern="(ss|gis)(?:\s|$)([\s\S]*)",
+    command=("ss", plugin_category),
+    info={
+        "header": "To Take a screenshot of a website.",
+        "usage": "{tr}ss <link>",
+        "examples": "{tr}ss https://github.com/TgCatUB/catuserbot",
+    },
 )
-async def _(event):
+async def screenshot(event):
+    "To Take a screenshot of a website."
     cmd = event.pattern_match.group(1)
     text = event.pattern_match.group(2)
     reply = await event.get_reply_message()
@@ -27,22 +32,22 @@ async def _(event):
     if not text and reply:
         text = reply.text
     if not text:
-        return await edit_delete(event, "اصبر بعد اخوك ...")
-    if cmd == "s;vk":
+        return await edit_delete(event, "`ثواني ...`")
+    if cmd == "ss":
         caturl = url(text)
         if not caturl:
             text = f"http://{text}"
             caturl = url(text)
         if not caturl:
-            return await edit_delete(event, "همممم هذا الرابط غير مدعوم")
-    elif cmd == "سكرنلي":
+            return await edit_delete(event, "`ما ادعم هيج روابط`")
+    elif cmd == "gis":
         text = f"https://www.google.com/search?q={text}"
-    batevent = await edit_or_reply(event, "يتم التحميل ...")
-    image, response = await chromeDriver.get_screenshot(text, batevent)
+    catevent = await edit_or_reply(event, "`Processing ...`")
+    image, response = await chromeDriver.get_screenshot(text, catevent)
     if not image:
-        return await edit_delete(batevent, response)
+        return await edit_delete(catevent, response)
 
-    await batevent.delete()
+    await catevent.delete()
     with io.BytesIO(image) as out_file:
         out_file.name = f"{text}.PNG"
         await event.client.send_file(
@@ -54,5 +59,60 @@ async def _(event):
             allow_cache=False,
             silent=True,
         )
-        
-      #جميع حقوق لسورس الخفاش تبوكه انيجك
+
+
+@catub.cat_cmd(
+    pattern="scapture ([\s\S]*)",
+    command=("scapture", plugin_category),
+    info={
+        "header": "To Take a screenshot of a website.",
+        "description": "For functioning of this command you need to set SCREEN_SHOT_LAYER_ACCESS_KEY var",
+        "usage": "{tr}scapture <link>",
+        "examples": "{tr}scapture https://github.com/TgCatUB/catuserbot",
+    },
+)
+async def scapture(event):
+    "To Take a screenshot of a website."
+    start = datetime.now()
+    message_id = await reply_id(event)
+    if Config.SCREEN_SHOT_LAYER_ACCESS_KEY is None:
+        return await edit_or_reply(
+            event,
+            "`Need to get an API key from https://screenshotlayer.com/product and need to set it SCREEN_SHOT_LAYER_ACCESS_KEY !`",
+        )
+    catevent = await edit_or_reply(event, "`يتم التحميل ...`")
+    sample_url = "https://api.screenshotlayer.com/api/capture?access_key={}&url={}&fullpage={}&viewport={}&format={}&force={}"
+    input_str = event.pattern_match.group(1)
+    inputstr = input_str
+    caturl = url(inputstr)
+    if not caturl:
+        inputstr = f"http://{input_str}"
+        caturl = url(inputstr)
+    if not caturl:
+        return await catevent.edit("`The given input is not supported url`")
+    response_api = requests.get(
+        sample_url.format(
+            Config.SCREEN_SHOT_LAYER_ACCESS_KEY, inputstr, "1", "2560x1440", "PNG", "1"
+        )
+    )
+    # https://stackoverflow.com/a/23718458/4723940
+    contentType = response_api.headers["content-type"]
+    end = datetime.now()
+    ms = (end - start).seconds
+    hmm = f"**url : **{input_str} \n**Time :** `{ms} seconds`"
+    if "image" in contentType:
+        with io.BytesIO(response_api.content) as screenshot_image:
+            screenshot_image.name = "screencapture.png"
+            try:
+                await event.client.send_file(
+                    event.chat_id,
+                    screenshot_image,
+                    caption=hmm,
+                    force_document=True,
+                    reply_to=message_id,
+                )
+                await catevent.delete()
+            except Exception as e:
+                await catevent.edit(str(e))
+    else:
+        await catevent.edit(f"`{response_api.text}`")
