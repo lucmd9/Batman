619b07aca587c0df255586bc40357a71992cd066
Angel <156003873+lucmd9@users.noreply.github.com>
2024-03-26 14:26:20 -0700

    Update سيكرون.py

--- BATT/plugins/سيكرون.py
+++ BATT/plugins/سيكرون.py
@@ -1,118 +1,62 @@
 #امر تاخذ سكرين للمواقع
 #————————BATT—————————-#LUC_MD9
-
 import io
+import traceback
 from datetime import datetime
-import requests
-from validators.url import url
-from BATT import lucmd9
-from ..Config import Config
-from ..core.managers import edit_delete, edit_or_reply
-from ..helpers.google_tools import chromeDriver
-from ..helpers.utils import reply_id
-
-plugin_category = "utils"
+from selenium import webdriver
+from telethon import events
+from userbot.utils import admin_cmd
 
 
-@lucmd9.ar_cmd(
-    pattern="(ss|gis)(?:\s|$)([\s\S]*)",
-    command=("ss", plugin_category),
-    info={
-        "header": "To Take a screenshot of a website.",
-        "usage": "{tr}ss <link>",
-        "examples": "{tr}ss https://github.com/TgCatUB/catuserbot",
-    },
-)
-async def screenshot(event):
-    "To Take a screenshot of a website."
-    cmd = event.pattern_match.group(1)
-    text = event.pattern_match.group(2)
-    reply = await event.get_reply_message()
-    reply_to_id = await reply_id(event)
-    if not text and reply:
-        text = reply.text
-    if not text:
-        return await edit_delete(event, "`ثواني ...`")
-    if cmd == "ss":
-        caturl = url(text)
-        if not caturl:
-            text = f"http://{text}"
-            caturl = url(text)
-        if not caturl:
-            return await edit_delete(event, "`ما ادعم هيج روابط`")
-    elif cmd == "gis":
-        text = f"https://www.google.com/search?q={text}"
-    catevent = await edit_or_reply(event, "`Processing ...`")
-    image, response = await chromeDriver.get_screenshot(text, catevent)
-    if not image:
-        return await edit_delete(catevent, response)
-
-    await catevent.delete()
-    with io.BytesIO(image) as out_file:
-        out_file.name = f"{text}.PNG"
-        await event.client.send_file(
-            event.chat_id,
-            out_file,
-            caption=response,
-            force_document=True,
-            reply_to=reply_to_id,
-            allow_cache=False,
-            silent=True,
-        )
-
-
-@catub.cat_cmd(
-    pattern="scapture ([\s\S]*)",
-    command=("scapture", plugin_category),
-    info={
-        "header": "To Take a screenshot of a website.",
-        "description": "For functioning of this command you need to set SCREEN_SHOT_LAYER_ACCESS_KEY var",
-        "usage": "{tr}scapture <link>",
-        "examples": "{tr}scapture https://github.com/TgCatUB/catuserbot",
-    },
-)
-async def scapture(event):
-    "To Take a screenshot of a website."
+@borg.on(admin_cmd("screenlong (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    if Config.GOOGLE_CHROME_BIN is None:
+        await event.edit("need to install Google Chrome. Module Stopping.")
+        return
+    await event.edit("Processing ...")
     start = datetime.now()
-    message_id = await reply_id(event)
-    if Config.SCREEN_SHOT_LAYER_ACCESS_KEY is None:
-        return await edit_or_reply(
-            event,
-            "`Need to get an API key from https://screenshotlayer.com/product and need to set it SCREEN_SHOT_LAYER_ACCESS_KEY !`",
-        )
-    catevent = await edit_or_reply(event, "`يتم التحميل ...`")
-    sample_url = "https://api.screenshotlayer.com/api/capture?access_key={}&url={}&fullpage={}&viewport={}&format={}&force={}"
-    input_str = event.pattern_match.group(1)
-    inputstr = input_str
-    caturl = url(inputstr)
-    if not caturl:
-        inputstr = f"http://{input_str}"
-        caturl = url(inputstr)
-    if not caturl:
-        return await catevent.edit("`The given input is not supported url`")
-    response_api = requests.get(
-        sample_url.format(
-            Config.SCREEN_SHOT_LAYER_ACCESS_KEY, inputstr, "1", "2560x1440", "PNG", "1"
-        )
-    )
-    # https://stackoverflow.com/a/23718458/4723940
-    contentType = response_api.headers["content-type"]
-    end = datetime.now()
-    ms = (end - start).seconds
-    hmm = f"**url : **{input_str} \n**Time :** `{ms} seconds`"
-    if "image" in contentType:
-        with io.BytesIO(response_api.content) as screenshot_image:
-            screenshot_image.name = "screencapture.png"
-            try:
-                await event.client.send_file(
-                    event.chat_id,
-                    screenshot_image,
-                    caption=hmm,
-                    force_document=True,
-                    reply_to=message_id,
-                )
-                await catevent.delete()
-            except Exception as e:
-                await catevent.edit(str(e))
-    else:
-        await catevent.edit(f"`{response_api.text}`")
+    try:
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--ignore-certificate-errors')
+        chrome_options.add_argument("--test-type")
+        chrome_options.add_argument("--headless")
+        # https://stackoverflow.com/a/53073789/4723940
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.binary_location = Config.GOOGLE_CHROME_BIN
+        await event.edit("Starting Google Chrome BIN")
+        driver = webdriver.Chrome(chrome_options=chrome_options)
+        input_str = event.pattern_match.group(1)
+        driver.get(input_str)
+        await event.edit("Calculating Page Dimensions")
+        height = driver.execute_script("return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);")
+        width = driver.execute_script("return Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);")
+        await event.edit("Painting web-page")
+        driver.set_window_size(width + 100, height + 100)
+        # تعديل ابعاد الصوره
+        # خوش ابعاد
+        im_png = driver.get_screenshot_as_png()
+        # saves screenshot of entire page
+        driver.close()
+        await event.edit("Stopping Google Chrome BIN")
+        message_id = event.message.id
+        if event.reply_to_msg_id:
+            message_id = event.reply_to_msg_id
+        with io.BytesIO(im_png) as out_file:
+            out_file.name = "@UniBorg.ScreenCapture.PNG"
+            await borg.send_file(
+                event.chat_id,
+                out_file,
+                caption=input_str,
+                force_document=True,
+                reply_to=message_id,
+                allow_cache=False,
+                silent=True
+            )
+        end = datetime.now()
+        ms = (end - start).seconds
+        await event.edit(f"Completed screencapture Process in {ms} seconds")
+    except Exception:
+        await event.edit(traceback.format_exc())
