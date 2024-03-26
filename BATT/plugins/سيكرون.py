bfb6af129dc9a42e1442e16604faf8632dd594fb
Angel <156003873+lucmd9@users.noreply.github.com>
2024-03-26 14:32:17 -0700

    Update سيكرون.py

--- BATT/plugins/سيكرون.py
+++ BATT/plugins/سيكرون.py
@@ -5,48 +5,43 @@ import traceback
 from datetime import datetime
 from selenium import webdriver
 from telethon import events
-from userbot.utils import admin_cmd
+from BATT.events import lucmd9
+from BATT.utils import edit_or_reply
 
+CHROME = "/usr/bin/google-chrome"
 
-@borg.on(admin_cmd("screenlong (.*)"))
-async def _(event):
+@lucmd9(pattern="سكرن (.*)")
+async def screenshot(event):
     if event.fwd_from:
         return
-    if Config.GOOGLE_CHROME_BIN is None:
-        await event.edit("need to install Google Chrome. Module Stopping.")
-        return
-    await event.edit("Processing ...")
+    await edit_or_reply(event, "اصبر....")
     start = datetime.now()
     try:
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--ignore-certificate-errors')
         chrome_options.add_argument("--test-type")
         chrome_options.add_argument("--headless")
-        # https://stackoverflow.com/a/53073789/4723940
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.binary_location = Config.GOOGLE_CHROME_BIN
-        await event.edit("Starting Google Chrome BIN")
-        driver = webdriver.Chrome(chrome_options=chrome_options)
-        input_str = event.pattern_match.group(1)
-        driver.get(input_str)
-        await event.edit("Calculating Page Dimensions")
+        chrome_options.binary_location = CHROME
+        await edit_or_reply(event, "يتم تشغيل كوكل  bin")
+        with webdriver.Chrome(options=chrome_options) as driver:
+            input_str = event.pattern_match.group(1)
+            driver.get(input_str)
+        await edit_or_reply(event, "يتم تجميع..")
         height = driver.execute_script("return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);")
         width = driver.execute_script("return Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);")
-        await event.edit("Painting web-page")
+        await edit_or_reply(event, "Painting web-page")
         driver.set_window_size(width + 100, height + 100)
-        # تعديل ابعاد الصوره
-        # خوش ابعاد
         im_png = driver.get_screenshot_as_png()
-        # saves screenshot of entire page
         driver.close()
-        await event.edit("Stopping Google Chrome BIN")
+        await edit_or_reply(event, "يتم ايقاف كروم bin")
         message_id = event.message.id
         if event.reply_to_msg_id:
             message_id = event.reply_to_msg_id
         with io.BytesIO(im_png) as out_file:
-            out_file.name = "@UniBorg.ScreenCapture.PNG"
-            await borg.send_file(
+            out_file.name = "BATT.screenCapture.PNG"
+            await event.client.send_file(
                 event.chat_id,
                 out_file,
                 caption=input_str,
@@ -57,6 +52,8 @@ async def _(event):
             )
         end = datetime.now()
         ms = (end - start).seconds
-        await event.edit(f"Completed screencapture Process in {ms} seconds")
+        await edit_or_reply(event, f"تم التقاط السكرين استغرق حوالي {ms} sec")
     except Exception:
-        await event.edit(traceback.format_exc())
+        await edit_or_reply(event, traceback.format_exc())
+
+# جميع الحقوق محفوظة لسورس الخفاش تبوكة انيجك
